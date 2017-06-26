9a30d215d8a7f561af96adc1397cd2d338c1db08
chenyn <1048196441@qq.com>
2017-06-26 13:52:04 +0800

    Initial Commit

---
+++ ARDemoSwift/ARDemoSwift-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ARDemoSwift-Bridging-Header.h
+//  ARDemoSwift
+//
+//  Created by chenyn on 2017/6/26.
+//  Copyright © 2017年 chenyn. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
